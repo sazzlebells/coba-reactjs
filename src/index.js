47948e47d9c99c475308ab0574b7a2f120d4f84e
Winnona Sarah <winnona.sarah@gmail.com>
2022-03-23 15:15:31 +0700

    navbar - pre: multiple components in sync - no parent child rls

--- src/index.js
+++ src/index.js
@@ -3,11 +3,10 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import Counters from "./components/counters";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Counters />
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
