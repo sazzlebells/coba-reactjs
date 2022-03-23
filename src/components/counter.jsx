f840b983df3785580be138b1fc8278af997db7dd
Winnona Sarah <winnona.sarah@gmail.com>
2022-03-23 07:22:25 +0700

    event parameter

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,46 +1,54 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-    state = {
-        count: 0,
-        // imgUrl: 'https://picsum.photos/200'
-        tags: ['tag1', 'tag2', 'tag3']
-    };
+  state = {
+    count: 0,
+    // imgUrl: 'https://picsum.photos/200'
+    tags: ["tag1", "tag2", "tag3"],
+  };
 
-    style = {
-        fontSize: 20,
-        fontWeight: "bold",
-    };
+  style = {
+    fontSize: 20,
+    fontWeight: "bold",
+  };
 
-    // constructor() {
-    //     super();
-    //     this.handleIncrement = this.handleIncrement.bind(this) //binding event handler cuz event cant access 'this'
-    // }
+  // constructor() {
+  //     super();
+  //     this.handleIncrement = this.handleIncrement.bind(this) //binding event handler cuz event cant access 'this'
+  // }
 
-    handleIncrement = () => {
-        this.setState({ count: this.state.count + 1 })
-    }
+  handleIncrement = (product) => {
+    console.log(product);
+    this.setState({ count: this.state.count + 1 });
+  };
 
-    render() {
+  render() {
+    return (
+      <React.Fragment>
+        {/* <img src={this.state.imgUrl} alt="" /> */}
+        <span style={this.style} className={this.getBadgeClasses()}>
+          {this.formatCount()}
+        </span>
+        <button
+          onClick={(product) => this.handleIncrement(product)}
+          style={{ fontSize: 15 }}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+      </React.Fragment>
+    );
+  }
 
-        return (
-            <React.Fragment>
-                {/* <img src={this.state.imgUrl} alt="" /> */}
-                <span style={this.style} className={this.getBadgeClasses()}>{this.formatCount()}</span>
-                <button onClick={this.handleIncrement} style={{ fontSize: 15 }} className="btn btn-secondary btn-sm">Increment</button>
-            </React.Fragment>
-        );
-    }
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+  }
 
-    getBadgeClasses() {
-        let classes = "badge m-2 badge-";
-        classes += (this.state.count === 0) ? "warning" : "primary";
-    }
-
-    formatCount() {
-        const { count } = this.state;
-        return count === 0 ? 'Zero' : count;
-    }
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
 }
 
 export default Counter;
