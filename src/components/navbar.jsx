07986a4bb2a9085265e8e57fd2f90ca6ef9a8b9d
Winnona Sarah <winnona.sarah@gmail.com>
2022-03-25 06:12:38 +0700

    destructuring arguments

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,13 +3,15 @@ import React, { Component } from "react";
 //stateless functional component
 //do this when you only have render function
 
-const NavBar = (props) => {
+const NavBar = ({ totalCounters }) => {
+  // const NavBar = (props) => {
   return (
     <nav className="navbar navbar-light bg-light">
       <a className="navbar-brand" href="#">
         NavBar{" "}
         <span className="badge badge-pill badge-secondary">
-          {props.totalCounters}
+          {totalCounters}
+          {/* {props.totalCounters} */}
         </span>
       </a>
     </nav>
