ff62f40d9268cd94ab4e9b01c9f424703e8c1fa0
Winnona Sarah <winnona.sarah@gmail.com>
2022-03-25 06:07:04 +0700

    lift up state

--- src/App.js
+++ src/App.js
@@ -1,20 +1,64 @@
-import React from "react";
+import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import NavBar from "./components/navbar";
 import Counters from "./components/counters";
+import { render } from "@testing-library/react";
 
-function App() {
-  return (
-    <div className="App">
-      <React.Fragment>
-        <NavBar />
-        <main className="container">
-          <Counters />
-        </main>
-      </React.Fragment>
-    </div>
-  );
+class App extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 0 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 },
+    ],
+  };
+
+  handleDelete = (counterId) => {
+    // console.log("Event handler called", counterId);
+    const counters = this.state.counters.filter((c) => c.id !== counterId);
+    this.setState({ counters });
+  };
+
+  handleIncrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    // console.log(this.state.counters[0]);
+    this.setState({ counters });
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map((c) => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <React.Fragment>
+          <NavBar
+            totalCounters={
+              this.state.counters.filter((c) => c.value > 0).length
+            }
+          />
+          <main className="container">
+            <Counters
+              counters={this.state.counters}
+              onReset={this.handleReset}
+              onDelete={this.handleDelete}
+              onIncrement={this.handleIncrement}
+            />
+          </main>
+        </React.Fragment>
+      </div>
+    );
+  }
 }
 
 export default App;
